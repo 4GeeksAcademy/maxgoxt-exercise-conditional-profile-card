51175224240fbc9e0d1579183e5b983917f93ae4
MaxGoxt <135187058+MaxGoxt@users.noreply.github.com>
2023-08-04 20:20:33 +0000

    commit

--- src/js/app.js
+++ src/js/app.js
@@ -39,16 +39,16 @@ function render(variables = {}) {
           <ul class="${variables.socialMediaPosition}">
             <li><a href="${
               variables.twitter
-            }"><i class="fab fa-twitter"></i></a></li>
+            }"><i class="fa fa-twitter"></i></a></li>
             <li><a href="${
               variables.github
-            }"><i class="fab fa-github"></i></a></li>
+            }"><i class="fa fa-github"></i></a></li>
             <li><a href="${
               variables.linkedin
-            }"><i class="fab fa-linkedin"></i></a></li>
+            }"><i class="fa fa-linkedin"></i></a></li>
             <li><a href="${
               variables.instagram
-            }"><i class="fab fa-instagram"></i></a></li>
+            }"><i class="fa fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
